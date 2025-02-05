4aa650657f1447e9cbc4d74e34a27e09f9372307
mariusschueller <mariuseschueller@gmail.com>
2025-02-05 14:43:19 -0600

    stl detection

--- api/api.py
+++ api/api.py
@@ -1,8 +1,14 @@
 from flask import Flask
 import subprocess
+import os
 
 
 app = Flask(__name__)
+
+def checkSTL(file):
+    if file.endswith(".stl"):
+        return True
+    return False
 
 @app.route('/')
 def home():
@@ -21,18 +27,25 @@ def home():
 
 @app.route('/file/<filename>')
 def file(filename):
-    result = subprocess.run(
-        [
-            "/home/marius/Documents/MATRIX/superslicer/superslicer",
-            "-g",
-            f"/home/marius/Documents/MATRIX/test_files/{filename}"
-        ],
-        capture_output=True,
-        text=True
-    )
-    output = result.stdout.split("\n")
-    error = result.stderr
-    return output
+    if checkSTL(filename):
+        file_path = f"/home/marius/Documents/MATRIX/test_files/{filename}"
+        if not os.path.exists(file_path):
+            return f"File {filename} does not exist", 404
+        
+        result = subprocess.run(
+            [
+                "/home/marius/Documents/MATRIX/superslicer/superslicer",
+                "-g",
+                f"/home/marius/Documents/MATRIX/test_files/{filename}"
+            ],
+            capture_output=True,
+            text=True
+        )
+        output = result.stdout.split("\n")
+        error = result.stderr
+        return output
+    else:
+        return "Invalid file format", 400
 
 if __name__ == '__main__':
     app.run(debug=True)
